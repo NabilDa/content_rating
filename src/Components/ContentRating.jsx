37a49d4584b911d4d36e7abce42a173d68f3c655
NabilDa <naabil.daoui@gmail.com>
2024-08-07 12:05:38 -0400

    Modify ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -3,16 +3,31 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            likes: 0,
+            dislikes: 0
+        }
+    }
+    render() {
+        return (
+            <>
+                <div className="content-rating">
+                    <p>
+                    Life is often seen as a complex journey, one that is impossible
+                    to understand, and this is the way I used to look at it for a long time.
+                    But, since I directed my efforts to reach inner peace—a state of mind
+                    where our mood is stable all of the time, a state of calmness
+                    that doesn’t change when we are going through difficult times,
+                    a state where what we crave is aligned with what our minds see
+                    as beneficial and meaningful—I noticed that seeing life as complex
+                    isn’t going to lead me to it.
+                    </p>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContentRating;
